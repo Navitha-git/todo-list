188df98b94a016480668c4e40345aff88deefd9c
Navitha-git <navitha.mukkamalla@gmail.com>
2023-02-04 21:42:13 +0530

    fixed update task bug

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -80,7 +80,6 @@ export default function CustomizedInputBase() {
         task: {
           ...editingTask,
           description: taskDescription,
-          priority: taskPriority,
         },
       })
         .then((response) => {
